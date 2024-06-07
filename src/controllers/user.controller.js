f5bf12498f6f9b4f7df485673445b5ffefdf1a45
AbhyudayadityaKumarSingh <abhyudayaditya.219302471@muj.manipal.edu>
2024-06-07 09:39:22 +0530

    made essential changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,14 @@
 import  {asyncHandler} from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.send(200).json({
-        message : "ok"
+    const [fullname,username, email, password] = req.body
+    res.status(200).json({
+        success: true,
+        message: "User registered successfully",
+        data: {
+            name,
+            email
+        }
     })
 })
 
