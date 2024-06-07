c8560de79dc0dd23b32fc1865957f0af8cbb761b
AbhyudayadityaKumarSingh <abhyudayaditya.219302471@muj.manipal.edu>
2024-06-07 10:03:54 +0530

    made changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,15 +1,24 @@
 import  {asyncHandler} from "../utils/asyncHandler.js";
 
-const registerUser = asyncHandler(async (req, res) => {
-    const [fullname,username, email, password] = req.body
-    res.status(200).json({
-        success: true,
-        message: "User registered successfully",
-        data: {
-            name,
-            email
-        }
-    })
-})
+// const registerUser = asyncHandler(async (req, res) => {
+//     const [fullname,username, email, password] = req.body
+//     res.status(200).json({
+//         success: true,
+//         message: "User registered successfully",
+//         data: {
+//             name,
+//             email
+//         }
+//     })
+// })
+
+const {fullName, username, email, password} = req.body
+console.log(email) 
+
+if (condition) {
+    
+} else {
+    
+}
 
 export {registerUser}
